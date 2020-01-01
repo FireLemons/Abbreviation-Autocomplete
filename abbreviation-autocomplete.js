8989a5b7b31337eeda4d1ec40dbd6e6509c40c2a
Shen Yang <shenyang64@gmail.com>
2019-12-31 18:54:01 -0600

    added ability to fill input with abbreviation

--- abbreviation-autocomplete.js
+++ abbreviation-autocomplete.js
@@ -27,6 +27,14 @@ Vue.component('abbreviation-autocomplete', {
       this.focused = false
     },
 
+    select: function() {
+      console.log(this.selected)
+      if(this.selected !== -1) {
+        this.input = this.searchList[this.selected].a
+        this.focused = false
+      }
+    },
+
     selectDown: function() {
       this.selected = (this.selected + 1) % this.searchList.length
     },
@@ -42,13 +50,12 @@ Vue.component('abbreviation-autocomplete', {
 
     setSelected(index){
       this.selected = index
-      console.log('index = ' + index)
     }
   },
   template: `
 <div class="abbreviation-autocomplete">
-  <input type="text" v-model="input" @focus="focused = true" @blur="onUnfocus" @keydown.down="selectDown" @keydown.up="selectUp">
-  <ul v-show="focused">
+  <input type="text" v-model="input" @focus="focused = true" @blur="onUnfocus" @keyup.enter="select" @keydown.down="selectDown" @keydown.up="selectUp">
+  <ul v-show="focused" @mousedown="select">
     <li v-for="(element, index) in searchList" :class="{ selected: index === selected }" @mouseover="setSelected(index)"><span>{{ element.a }}</span><span> ({{ element.def }})</span></li>
   </ul>
 </div>
