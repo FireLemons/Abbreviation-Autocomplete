83ed3f8de1cb596ead9fb20d24567ff8fefa1268
firelemons <shenyang64@gmail.com>
2020-02-07 13:38:39 -0600

    renamed input to searchText

--- abbreviation-autocomplete.js
+++ abbreviation-autocomplete.js
@@ -54,7 +54,7 @@ Vue.component('abbreviation-autocomplete', {
   data: function () {
     return {
       focused: false,
-      input: '',
+      searchText: '',
       loading: false,
       recentlySelected: false,
       searchList: [],
@@ -68,26 +68,26 @@ Vue.component('abbreviation-autocomplete', {
       default: Infinity,
       type: Number
     },
-    minInputLength: {
+    minSearchTextLength: {
       default: 1,
       type: Number
     }
   },
   watch: {
-    input: function () {
-      this.onInputChange()
+    searchText: function () {
+      this.onSearchTextChange()
     }
   },
   methods: {
     loadRelatedItems: function () {
-      if (this.input.length >= this.minInputLength) {
+      if (this.searchText.length >= this.minSearchTextLength) {
         const countingSortData = []
         const relatedResults = []
 
         this.data.forEach((elem) => {
-          const index = elem.d.toLowerCase().indexOf(this.input.toLowerCase())
+          const index = elem.d.toLowerCase().indexOf(this.searchText.toLowerCase())
 
-          // if user input is a substring of this definition
+          // if search text is a substring of this definition
           if (index >= 0) {
             countingSortInsert(relatedResults, countingSortData, elem, index)
             elem.substrIndex = index
@@ -100,7 +100,7 @@ Vue.component('abbreviation-autocomplete', {
         return []
       }
     },
-    onInputChange: function () {
+    onSearchTextChange: function () {
       if (this.recentlySelected) {
         this.recentlySelected = false
       } else {
@@ -108,7 +108,7 @@ Vue.component('abbreviation-autocomplete', {
         this.selected = -1
       }
 
-      if (this.input.length >= this.minInputLength) {
+      if (this.searchText.length >= this.minSearchTextLength) {
         this.loading = true
         this.searchList = this.loadRelatedItems()
       } else {
@@ -128,7 +128,7 @@ Vue.component('abbreviation-autocomplete', {
     select: function () {
       if (this.selected !== -1) {
         this.focused = false
-        this.input = this.searchList[this.selected].a
+        this.searchText = this.searchList[this.selected].a
         this.recentlySelected = true
       }
     },
@@ -152,7 +152,7 @@ Vue.component('abbreviation-autocomplete', {
   },
   template: `
 <div class="abbreviation-autocomplete">
-  <input type="text" v-model="input" @focus="focused = true" @blur="onUnfocus" @keyup.enter="select" @keydown.down="selectDown" @keydown.up="selectUp">
+  <input type="text" v-model="searchText" @focus="focused = true" @blur="onUnfocus" @keyup.enter="select" @keydown.down="selectDown" @keydown.up="selectUp">
   <ul v-show="focused" @mousedown="select">
     <li v-show="loading">
       <svg height="10" width="10">
@@ -167,7 +167,7 @@ Vue.component('abbreviation-autocomplete', {
     </li>
     <li v-for="(element, index) in searchList" :class="{ selected: index === selected }" @mouseover="setSelected(index)">
       <span>{{ element.a }}</span>
-      <span> ({{ element.d.substr(0, element.substrIndex) }}</span><span class="highlight">{{ input }}</span><span>{{ element.d.substr(element.substrIndex + input.length) }})</span>
+      <span> ({{ element.d.substr(0, element.substrIndex) }}</span><span class="highlight">{{ searchText }}</span><span>{{ element.d.substr(element.substrIndex + searchText.length) }})</span>
     </li>
   </ul>
 </div>
@@ -183,8 +183,8 @@ Vue.component('abbreviation-autocomplete', {
 
     // Only emit if there's a listener attached on creation
     if (listeners) {
-      if (listeners['update:input']) {
-        this.onInputChange = () => {
+      if (listeners['update:searchText']) {
+        this.onSearchTextChange = () => {
           if (this.recentlySelected) {
             this.recentlySelected = false
           } else {
@@ -192,7 +192,7 @@ Vue.component('abbreviation-autocomplete', {
             this.selected = -1
           }
 
-          if (this.input.length >= this.minInputLength) {
+          if (this.searchText.length >= this.minSearchTextLength) {
             this.loading = true
             this.searchList = this.loadRelatedItems()
           } else {
@@ -200,7 +200,7 @@ Vue.component('abbreviation-autocomplete', {
             this.searchList = []
           }
 
-          this.$emit('update:input', this.input)
+          this.$emit('update:searchText', this.searchText)
         }
       }
 
@@ -211,7 +211,7 @@ Vue.component('abbreviation-autocomplete', {
             // clear key used for sorting autocomplete results
             delete this.searchList[this.selected].substrIndex
             this.$emit('select', this.searchList[this.selected])
-            this.input = this.searchList[this.selected].a
+            this.searchText = this.searchList[this.selected].a
             this.recentlySelected = true
           }
         }
